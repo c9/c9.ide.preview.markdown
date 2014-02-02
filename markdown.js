a048ed9b27a0551c814279e1aed5fae705505f4c
Ruben Daniels <ruben@c9.io>
2014-02-02 17:04:28 +0100

    Working on html live updating

--- markdown.js
+++ markdown.js
@@ -1,113 +1,145 @@
 define(function(require, exports, module) {
-    main.consumes = ["plugin", "menus", "preview", "tabs", "ui"];
+    main.consumes = [
+        "c9", "Previewer", "fs", "dialog.error", "commands", "tabManager"
+    ];
     main.provides = ["preview.markdown"];
     return main;
 
+    // @todo possible improvements: http://benweet.github.io/stackedit/#
+
     function main(options, imports, register) {
-        var Plugin   = imports.plugin;
-        var preview  = imports.preview;
-        var tabs     = imports.tabs;
-        var ui       = imports.ui;
-        var menus    = imports.menus;
-        // var Menu     = menus.Menu;
-        var MenuItem = menus.MenuItem;
-        var Divider  = menus.Divider;
+        var Previewer  = imports.Previewer;
+        var c9         = imports.c9;
+        var fs         = imports.fs;
+        var commands   = imports.commands;
+        var tabManager = imports.tabManager;
+        var showError  = imports["dialog.error"].show;
+        var dirname    = require("path").dirname;
         
         /***** Initialization *****/
         
-        var plugin = new Plugin("Ajax.org", main.consumes);
-        // var emit   = plugin.getEmitter();
-        
-        const HTMLURL = options.htmlurl || location.protocol + "//" 
-            + location.host + "/static/plugins/c9.ide.preview.markdown/index.html"
-        
-        var loaded = false;
-        function load(){
-            if (loaded) return false;
-            loaded = true;
-            
-            preview.register(plugin, function(path){
+        var plugin = new Previewer("Ajax.org", main.consumes, {
+            caption  : "Markdown",
+            index    : 200,
+            selector : function(path){
                 return path.match(/(?:\.md|\.markdown)$/i);
-            });
-            
-            var menu = preview.previewMenu;
-            menu.append(new MenuItem({ 
-                caption  : "Markdown", 
-                position : 200,
-                onclick  : function(){
-                    // The menu can only be shown if the previewer has focus
-                    var editor = tabs.focussedPage.editor;
-                    editor.setPreviewer("preview.markdown");
-                }
-            }));
-        }
+            }
+        });
+        var emit = plugin.getEmitter();
+        
+        var HTMLURL, previewOrigin;
         
         /***** Methods *****/
         
-        function loadDocument(doc, preview){
+        function getPreviewUrl(fn){
+            if (options.local && document.baseURI.substr(0, 5) == "file:")
+                return setTimeout(getPreviewUrl.bind(null, fn), 100);
+            else if (HTMLURL)
+                return fn(HTMLURL);
+            
+            HTMLURL = (options.staticPrefix 
+                ? (options.local ? dirname(document.baseURI) + "/static" : options.staticPrefix) 
+                    + options.htmlPath
+                : "/static/plugins/c9.ide.preview/previewers/markdown.html")
+                    + "?host=" + (options.local ? "local" : location.origin);
+                
+            if (HTMLURL.charAt(0) == "/")
+                HTMLURL = location.protocol + "//" + location.host + HTMLURL;
+    
+            previewOrigin = HTMLURL.match(/^(?:[^\/]|\/\/)*/)[0];
+            
+            fn(HTMLURL);
+        }
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function(){
+            
+        });
+        plugin.on("documentLoad", function(e){
+            var doc     = e.doc;
             var session = doc.getSession();
-            var page    = doc.page;
+            var tab     = doc.tab;
+            var editor  = e.editor;
             
             var iframe = document.createElement("iframe");
+            
+            // iframe.setAttribute("nwfaketop", true);
+            iframe.setAttribute("nwdisable", true);
+
             iframe.style.width    = "100%";
             iframe.style.height   = "100%";
             iframe.style.border   = 0;
             iframe.style.backgroundColor = "rgba(255, 255, 255, 0.88)";
             
-            session.iframe = iframe;
-            preview.container.appendChild(session.iframe);
-            
-            window.addEventListener("message", function(e){
-                // if (event.origin !== "http://example.org:8080")
-                //     return;
-                
-                if (e.data.message == "stream.document") {
-                    session.source = e.source;
-                    session.source.postMessage({
-                        type    : "document",
-                        content : session.previewPage.document.value
-                    }, location.origin);
-                    
-                    page.className.remove("loading");
+            if (options.local) {
+                iframe.onload = function(){
+                    plugin.activeSession.add(iframe.contentWindow.location.href);
                 }
-            }, false);
+            }
+            
+            var onMessage = function(e) {
+                if (c9.hosted && event.origin !== previewOrigin)
+                    return;
+                
+                if (e.data.message == "exec") {
+                    commands.exec(e.data.command);
+                }
+                else if (e.data.message == "focus") {
+                    tabManager.focusTab(tab);
+                }
+                else if (e.data.message == "stream.document") {
+                    session.source = e.source;
+                    
+                    if (session.previewTab) {
+                        var doc = session.previewTab.document;
+                        
+                        session.source.postMessage({
+                            type    : "document",
+                            content : doc.value
+                        }, "*");
+                        
+                        if (!doc.hasValue())
+                            doc.once("setValue", function(){
+                                emit("update", { previewDocument: doc });
+                            });
+                    }
+                    else {
+                        fs.readFile(session.path, function(err, data){
+                            if (err)
+                                return showError(err.message);
+                            
+                            session.source.postMessage({
+                                type    : "document",
+                                content : data
+                            }, "*");
+                        });
+                    }
+                    
+                    session.source.postMessage({
+                        type : "keys",
+                        keys : commands.getExceptionBindings()
+                    }, "*");
+                    
+                    tab.className.remove("loading");
+                }
+            };
+            window.addEventListener("message", onMessage, false);
+            session.addOther(function(){
+                window.removeEventListener("message", onMessage, false);
+            });
+            
+            // Set iframe
+            session.iframe = iframe;
             
             // Load the markup renderer
-            iframe.src = HTMLURL;
+            getPreviewUrl(function(url){ iframe.src = url; });
             
-            session.on("navigate", function(e){
-                page.className.add("loading");
-                
-                page.title    = 
-                page.tooltip  = "[M] " + e.url;
-                preview.getElement("txtPreview").setValue(e.url);
-                
-                iframe.src = iframe.src;
-            }, session);
-            session.on("update", function(e){
-                if (!session.source) return; // Renderer is not loaded yet
-                
-                session.source.postMessage({
-                    type    : "document",
-                    content : session.previewPage.document.value
-                }, location.origin);
-            }, session);
-            session.on("reload", function(e){
-                page.className.add("loading");
-                iframe.src = iframe.src;
-            }, session);
-            session.on("activate", function(){
-                session.iframe.style.display = "block";
-                preview.getElement("txtPreview").setValue(session.path);
-                preview.getElement("btnMode").setCaption("Markdown");
-                preview.getElement("btnMode").setIcon("page_white.png");
-            }, session);
-            session.on("deactivate", function(){
-                session.iframe.style.display = "none";
-            }, session);
-        }
-        
-        function unloadDocument(doc){
+            session.editor = editor;
+            editor.container.appendChild(session.iframe);
+        });
+        plugin.on("documentUnload", function(e){
+            var doc     = e.doc;
             var session = doc.getSession();
             var iframe  = session.iframe;
             iframe.parentNode.removeChild(iframe);
@@ -115,13 +147,46 @@ define(function(require, exports, module) {
             if (session.onchange)
                 session.pdoc.undoManager.off("change", session.onchange);
             
-            doc.page.className.remove("loading");
-        }
-        
-        /***** Lifecycle *****/
-        
-        plugin.on("load", function(){
-            load();
+            doc.tab.className.remove("loading");
+        });
+        plugin.on("documentActivate", function(e){
+            var session = e.doc.getSession();
+            
+            session.iframe.style.display = "block";
+            session.editor.setLocation(session.path);
+            session.editor.setButtonStyle("Markdown", "page_white.png");
+        });
+        plugin.on("documentDeactivate", function(e){
+            var session = e.doc.getSession();
+            session.iframe.style.display = "none";
+        });
+        plugin.on("navigate", function(e){
+            var tab    = plugin.activeDocument.tab;
+            var iframe = plugin.activeSession.iframe;
+            var editor = plugin.activeSession.editor;
+            
+            tab.className.add("loading");
+            
+            tab.title    = 
+            tab.tooltip  = "[M] " + e.url;
+            editor.setLocation(e.url);
+            
+            iframe.src = iframe.src;
+        });
+        plugin.on("update", function(e){
+            var session = plugin.activeSession;
+            if (!session.source) return; // Renderer is not loaded yet
+    
+            session.source.postMessage({
+                type    : "document",
+                content : e.previewDocument.value
+            }, "*");
+        });
+        plugin.on("reload", function(){
+            var iframe = plugin.activeSession.iframe;
+            var tab    = plugin.activeDocument.tab;
+            tab.className.add("loading");
+            iframe.src = iframe.src;
         });
         plugin.on("enable", function(){
             
@@ -130,27 +195,14 @@ define(function(require, exports, module) {
             
         });
         plugin.on("unload", function(){
-            loaded = false;
-            // drawn  = false;
         });
         
         /***** Register and define API *****/
         
         /**
-         * Draws the file tree
-         * @event afterfilesave Fires after a file is saved
-         *   object:
-         *     node     {XMLNode} description
-         *     oldpath  {String} description
+         * Previewer for markdown content.
          **/
         plugin.freezePublicAPI({
-            /**
-             */
-            loadDocument : loadDocument,
-            
-            /**
-             */
-            unloadDocument : unloadDocument
         });
         
         register(null, {
